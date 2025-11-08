474b604940e6557183aa6da9fa354d82af80d422
Tobias Rojahn <tobias@rojahn.org>
2025-11-08 12:29:04 +0100

    adding trap and goal and moving entities

--- scripts/goal.gd
+++ scripts/goal.gd
@@ -1,6 +1,5 @@
-extends Node2D
+extends Area2D
 
-@export var start_level: PackedScene;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,7 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_button_button_up() -> void:
-	get_tree().change_scene_to_packed(start_level)
