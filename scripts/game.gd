238e668848d230797ffb793ae650dd95e0d80d3e
Tobias Rojahn <tobias@rojahn.org>
2025-11-08 16:21:49 +0100

    add start function

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,8 +6,18 @@ var COMMAND_DURATION = DIFFICULTY_SETTINGS.Easy
 
 var paused = false
 
+func _process(delta: float) -> void:
+	pass
+	
+func start(difficulty: float) -> void:
+	COMMAND_DURATION = difficulty;
+	get_tree().change_scene_to_file("res://scenes/levels/level_1.tscn");
+	paused = false;
+
 func lose() -> void:
 	get_tree().reload_current_scene();
+	paused = false;
 	
 func win(next_level: PackedScene) -> void:
 	get_tree().change_scene_to_packed(next_level);
+	paused = false;
